8b0467db9d26e09b7b083a79be705df7cd1799da
Bradley Whitlock <bradley.whitlock@rubikloud.com>
2017-03-29 23:55:55 -0400

    add data, improve algorithm

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-# This is the main runner
 import math
+from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
+import os 
+import csv
 
 # Parameters: theta - Vector of coefficients
 #             x     - 1-D Vector of a specific training example
+# Vectors must be of the same length
 def hyp(x, theta):
 	z = 0
 	for t in theta:
@@ -13,34 +18,91 @@ def hyp(x, theta):
 # Reprents the "error" in the presumed function. This is what is minimized (line error)
 # Parameters: x - 2-D Vector representing the training data
 #             y - 1-D Vector of real numbers representing results
-#			  t - 1-D Vector of coefficients
-def cost(x, y, t):
+#			  theta - 1-D Vector of coefficients
+def cost(x, y, theta):
 	summation = 0
 	for i in range(x):
-		summation += y[i]*math.log(hyp(x[i],t),10) + (1-y[i])*math.log(1-hyp(x[i],t),10)
+		summation += y[i]*math.log(hyp(x[i],theta),10) + (1-y[i])*math.log(1-hyp(x[i],theta),10)
 	return -summation/len(x)
 
 # Parameters: x - 2-D Vector of training data
 #             y - 1-D Vector of real numbers representing results
-#			  t - 1-D Vector of coefficients
+#			  theta - 1-D Vector of coefficients
 # 			  alpha - Learning rate	meters: 
 def step(x, y, theta, alpha):
-	print "hello"
+	total = 0
+	temp_theta = []
+	for j in range(0, len(x[0])):
+		for i in range(0, x):
+			total += x_data[i][j] * (hyp(x[i], theta) - y[i])
+		temp_theta.append(theta[j] - alpha*total/float(len(x)))
+		total = 0
+	return temp_theta
 
 # Parameters: x - 2-D Vector of training data
 #             y - 1-D Vector of real numbers representing results
-#			  t - 1-D Vector of coefficients
+#			  theta - 1-D Vector of coefficients
 # 			  max_itt - Spacify the number of times "theta" is updated
 # 			  alpha - Learning rate	
 def gradient_descent_runner(x, y, theta, max_itt, alpha):
 	last_cost = cost(x, y, theta)
+	count = 0
 	for i in range(max_itt):
 		theta = step(x, y, theta, alpha)
+		curr_cost = cost(x, y, t)
+		if last_cost < curr_cost:
+			break
+		last_cost = curr_cost
+		count += 1
+	print "Number of itterations (debugging): ", count
+	return theta
 
+# Parameters: file_name - The name of the file that will populate our data
+def populate(file_name):
+    x = []
+    y = []
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(dir_path + '/' + file_name, 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter= ',')
+        reader = float_wrapper(reader)
+        for row in reader:
+            # This is for completeness, first factor is always 1
+            x.append([1])
+            x[len(X) - 1] += row[0:len(row) - 1]
+            y.append(row[len(row) - 1])
+    return (x, y)
 
+def plot(x, y):
+	print "this is the plot"
+
+# Notes:
+# 	- Must have at least 1 training set
 def run():
 	print "Wow Logistic Regression is Easy and Fun"
+	x, y = populate('grain_size.csv')
+	theta = [0] * len(x[0])
 
+	alpha = 0.001
+	max_itt = 1
+
+	print 'x: ', x
+	print 'y: ', y
+	print 'Running...'
+	theta = gradient_descent_runner(x, y, theta, max_itt, alpha)
+
+	plot(x, y, theta)
 
 if __name__ == '__main__':
     run()
+
+
+######################################################################################################
+# Notes
+# In logistic regression the Y {0,1}
+# The reason for the different cost function than linear regression is becasue
+#	the new cost function guarantees a convex shape, 1 local min is necessary
+#	for gradient descent to work properly
+# The differance between linear and logistic is the hypothesis. The gradient descent is the same
+# Feature scaling exists for logistic regression as well
+# Logistic Regression can be applied to nominal values (non numberic) which is an advantage to linear
+# 	The nominal value can also be y (the result)
